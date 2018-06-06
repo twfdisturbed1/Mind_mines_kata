436cee24cd637556ff80a523ab8c7296ce493365
William Mullins <twfdisturbed@hotmail.com>
2018-06-06 12:42:54 -0400

    Test

---
+++ print_to_screen.rb
@@ -0,0 +1,25 @@
+# p 1
+# puts 1
+# print 1
+# 5.times do 
+#     puts 1
+# end
+
+number = 1
+
+100.times do 
+    if number == 3
+        puts "mind"
+    else
+        
+    end
+    if number == 100
+        puts " Almost Done !!"
+    end
+
+    puts number
+    number = number + 1    
+end
+
+
+
